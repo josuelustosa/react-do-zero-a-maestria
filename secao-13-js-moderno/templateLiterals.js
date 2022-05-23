f04f1539311976369481ed4ed1b32dcfd5a51423
Josué Lustosa <josue.filho@conecthus.org.br>
2022-05-23 12:11:10 -0300

    :construction: template literals

---
+++ secao-13-js-moderno/templateLiterals.js
@@ -0,0 +1,7 @@
+// 5. Template Literals
+console.log('5. Template Literals')
+
+const userName = '@josuelustosa'
+const age = 21
+
+console.log(`O nome de usuário é ${userName} e sua idade é ${age}.`)
