2100f64c48342ea05a5a44dfc004b1bf8ac1128e
Josué Lustosa <josue.filho@conecthus.org.br>
2022-05-23 11:59:19 -0300

    :construction: map method

--- secao-13-js-moderno/map.js
+++ secao-13-js-moderno/map.js
@@ -1,2 +1,17 @@
 // 4. Método Map
 console.log('4. Método Map')
+
+const products = [
+    {name: 'Camisa', price: 19.99, category: 'Roupas'},
+    {name: 'Air Fryer', price: 249.99, category: 'Eletro'},
+    {name: 'Fogão', price: 400, category: 'Eletro'},
+    {name: 'Calça Jeans', price: 69.99, category: 'Roupas'}
+]
+
+products.map((product) => {
+    if (product.category === 'Roupas') {
+        product.onSale = true
+    }
+})
+
+console.log(products)
