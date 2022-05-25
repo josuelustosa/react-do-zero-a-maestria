92d801a8c86c04afd9851f27125cccdd52f7099e
Josué Lustosa <josue.filho@conecthus.org.br>
2022-05-25 17:39:40 -0400

    :tada: initial commit - creat app react

---
+++ secao-1-introducao/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World com React utilizando <code>npx create-react-app hello-world</code>.</h1>
+      <h3>Saiba mais <a href="https://pt-br.reactjs.org/docs/create-a-new-react-app.html" target="_blank" rel="noopener noreferrer">AQUI.</a> </h3>
+      <p>Desenvolvido por: <a href="http://github.com/josuelustosadev" target="_blank" rel="noopener noreferrer">Josué Lustosa | Frontend Developer</a> </p>
+    </div>
+  );
+}
+
+export default App;
