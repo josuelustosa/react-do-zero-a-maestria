314964641dd5ca53b2e2689246567bf37b642cbd
Josué <josue.filho@conecthus.org.br>
2022-06-02 10:51:50 -0400

    :construction: classroom - img in public and src

--- secao-3-avancando-em-react/avancando-em-react/src/App.js
+++ secao-3-avancando-em-react/avancando-em-react/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+import City from './assets/city.jpg';
+
 function App() {
   return (
     <div>
       <h1>Avançando em React</h1>
+
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem com pôr do Sol" />
+      </div>
+
+      {/* Imagem em src */}
+      <div>
+        <img src={City} alt="Cidade a noite" />
+      </div>
+
     </div>
   );
 }
