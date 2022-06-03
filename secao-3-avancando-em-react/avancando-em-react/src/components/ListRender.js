e5512670a703b5790b0a6ecf30c059c0417fdd80
Josué <josue.filho@conecthus.org.br>
2022-06-03 16:33:28 -0400

    :construction:

--- secao-3-avancando-em-react/avancando-em-react/src/components/ListRender.js
+++ secao-3-avancando-em-react/avancando-em-react/src/components/ListRender.js
@@ -8,12 +8,26 @@ const ListRender = () => {
       "Areta",
   ]);
 
+  const [users] = useState([
+      {id: 1, name: "Josué", age: 51},
+      {id: 2700001, name: "Waldiza", age: 49},
+      {id: 2100001, name: "Josué Filho", age: 21},
+      {id: 27, name: "Areta", age: 18},
+  ]);
+
   return (  
     <div>
-        <p>Lista de Usuários do Sistema</p>
+        <p>Listagem sem propriedade key</p>
         <ul>
             {list.map((item) => (
                 <li>{item}</li>
+            ))}
+        </ul>
+
+        <p>Lista de Usuários do Sistema com propriedade key no objeto</p>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>{user.name} - {user.age}</li>
             ))}
         </ul>
     </div>
